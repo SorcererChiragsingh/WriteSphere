2f07240d86f8d6b01b2b46ae4ef434daa2598022
Chirag singh <chiragsingh8926@gmail.com>
2025-04-06 21:00:42 +0530

    Working on Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Layout/Layout'
+import Index from './pages/Index'
+import { RouteIndex } from './helpers/RouteName'
+
 
 const App = () => {
   return (
-    <div>
 
-      <Button>Press me!!!!</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={RouteIndex} element={<Layout />} >
+          <Route index element={<Index />} />
+
+          </Route>
+          </Routes>
+    </BrowserRouter>
 
   )
 }
