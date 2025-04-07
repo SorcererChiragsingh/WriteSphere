00f420a7186bfeb9d7d34e4e8f3c9e53ef88cb72
Chirag singh <chiragsingh8926@gmail.com>
2025-04-07 22:42:21 +0530

    Add more components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from './components/ui/button'
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import Index from './pages/Index'
 import { RouteIndex } from './helpers/RouteName'
